22c81d7d7f36a0f921967a6c5b4a27e4ef5bdd64
Sym <satang.tang450@gmail.com>
2026-07-18 14:08:04 +0700

    +edit show_catalog.py

--- weapon_shop/show_catalog.py
+++ weapon_shop/show_catalog.py
@@ -7,8 +7,10 @@ from data import weapons_catalog
 def show_catalog():
 #   - print อาวุธทุกชิ้นใน weapons_catalog บรรทัดละชิ้น (รหัส, ชื่อ, ราคา, พลังโบนัส)
     # TODO: เขียนโค้ดตรงนี้
-    pass
-
+    print("========================== WEAPON CATALOG ==========================")
+    for i, weapon in weapons_catalog.items():
+        print(f"CODE ({i}) | NAME | {weapon['name']} | PRICE {weapon['price']}$ | POWER_BONUS -> {weapon['bonus']} |")
+    print("====================================================================")
 
 # ทดสอบเฉพาะไฟล์ตัวเอง: พิมพ์  python -m weapon_shop.show_catalog
 if __name__ == "__main__":
