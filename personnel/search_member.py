bb78cb71c10c3abee8dd747262c88f33cf7c246a
LDLydia <ld.lydia555@gmail.com>
2026-07-18 14:04:27 +0700

    edit search/show by Lydia

--- personnel/search_member.py
+++ personnel/search_member.py
@@ -1,14 +1,18 @@
 # =====================================================
 #  personnel/search_member.py — คนรับผิดชอบ: ______________________
 #  หน้าที่: ค้นหาลูกน้องจากชื่อ
-# =====================================================
+# =================================================
 from data import family_members
 
 def search_member(target_name):
 #   - หาคนใน family_members ที่ชื่อตรงกับ target_name (ไม่สนตัวพิมพ์ใหญ่/เล็ก)
 #   - เจอ -> return dict ของคนนั้น | ไม่เจอ -> return None
-    # TODO: เขียนโค้ดตรงนี้
-    pass
+    for member in family_members:
+        if member["name"].lower() == target_name.lower():
+            return member
+        return None
+        
+    
 
 
 # ทดสอบเฉพาะไฟล์ตัวเอง: พิมพ์  python -m personnel.search_member
